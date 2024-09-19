34a76acfc736d24bb8a42374454bcf686868419e
Praneshxo <praneshgara@gmail.com>
2024-09-19 05:43:59 +0530

    19

--- src/pages/Payroll.js
+++ src/pages/Payroll.js
@@ -4,11 +4,7 @@ import React from 'react';
 const Payroll = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
-      {/* Add more dashboard content here */}
-      <div>
-        <p>Payroll</p>
-      </div>
+    <div className='dhi'></div>
     </div>
   );
 };
